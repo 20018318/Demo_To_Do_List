6c17ac0fc89e7cf272079853492233463a84bded
20018318 <20018318@myrp.edu.sg>
2021-07-26 12:54:19 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20018318/demotodolist/CustomAdapter.java
@@ -0,0 +1,42 @@
+package sg.edu.rp.c346.id20018318.demotodolist;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class CustomAdapter extends ArrayAdapter {
+    Context parent_context;
+    int layout_id;
+    ArrayList<ToDoItem> versionList;
+
+    public CustomAdapter( Context context, int resource, ArrayList<ToDoItem> objects) {
+        super(context, resource, objects);
+
+        parent_context = context;
+        layout_id = resource;
+        versionList = objects;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater)
+                parent_context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View rowView = inflater.inflate(layout_id, parent, false);
+
+        TextView tvName = rowView.findViewById(R.id.tvName);
+        TextView tvVersion = rowView.findViewById(R.id.tvVersion);
+
+        ToDoItem currentVersion = versionList.get(position);
+
+        tvName.setText(currentVersion.getTitle());
+        tvVersion.setText(currentVersion.toString());
+
+        return rowView;
+    }
+}
